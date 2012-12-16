14430bb60c5cfb64b8417fcb7fd15cea1caf43b0
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2012-12-16 11:11:56 +1100

    Group CSS class tests

--- specs/BespokeSpec.js
+++ specs/BespokeSpec.js
@@ -37,69 +37,85 @@
 				});
 			});
 
-			it("should add a 'bespoke-active' class to the active slide", function() {
-				presentation.activate(3);
-				expect(slides[3].className).toMatch(/bespoke-active(\s|$)/);
-			});
+			describe("bespoke-active", function() {
 
-			it("should not add a 'bespoke-active' class to all inactive slides", function() {
-				slides = slides.reverse().slice(0, slides.length - 2).reverse();
-
-				slides.forEach(function(slide) {
-					expect(slide.className).not.toMatch(/bespoke-active(\s|$)/);
+				it("should add a 'bespoke-active' class to the active slide", function() {
+					presentation.activate(3);
+					expect(slides[3].className).toMatch(/bespoke-active(\s|$)/);
 				});
-			});
 
-			it("should add a 'bespoke-inactive' class to all inactive slides", function() {
-				slides = slides.reverse().slice(0, slides.length - 2).reverse();
+				it("should not add a 'bespoke-active' class to all inactive slides", function() {
+					slides = slides.reverse().slice(0, slides.length - 2).reverse();
 
-				slides.forEach(function(slide) {
-					expect(slide.className).toMatch(/bespoke-inactive(\s|$)/);
+					slides.forEach(function(slide) {
+						expect(slide.className).not.toMatch(/bespoke-active(\s|$)/);
+					});
 				});
+
 			});
 
-			it("should not add a 'bespoke-inactive' class to the active slide", function() {
-				expect(slides[0].className).not.toMatch(/bespoke-inactive(\s|$)/);
-			});
+			describe("bespoke-inactive", function() {
 
-			it("should add a 'bespoke-before' class to all slides before active slide", function() {
-				presentation.activate(5);
-				
-				var beforeSlides = slides.slice(0, 4);
+				it("should add a 'bespoke-inactive' class to all inactive slides", function() {
+					slides = slides.reverse().slice(0, slides.length - 2).reverse();
 
-				beforeSlides.forEach(function(slide) {
-					expect(slide.className).toMatch(/bespoke-before(\s|$)/);
+					slides.forEach(function(slide) {
+						expect(slide.className).toMatch(/bespoke-inactive(\s|$)/);
+					});
 				});
+
+				it("should not add a 'bespoke-inactive' class to the active slide", function() {
+					expect(slides[0].className).not.toMatch(/bespoke-inactive(\s|$)/);
+				});
+
 			});
 
-			it("should not add a 'bespoke-before' class to all slides after active slide", function() {
-				presentation.activate(5);
-				
-				var notBeforeSlides = slides.slice(5, 9);
+			describe("bespoke-before", function() {
 
-				notBeforeSlides.forEach(function(slide) {
-					expect(slide.className).not.toMatch(/bespoke-before(\s|$)/);
+				it("should add a 'bespoke-before' class to all slides before active slide", function() {
+					presentation.activate(5);
+					
+					var beforeSlides = slides.slice(0, 4);
+
+					beforeSlides.forEach(function(slide) {
+						expect(slide.className).toMatch(/bespoke-before(\s|$)/);
+					});
 				});
+
+				it("should not add a 'bespoke-before' class to all slides after active slide", function() {
+					presentation.activate(5);
+					
+					var notBeforeSlides = slides.slice(5, 9);
+
+					notBeforeSlides.forEach(function(slide) {
+						expect(slide.className).not.toMatch(/bespoke-before(\s|$)/);
+					});
+				});
+
 			});
 
-			it("should add a 'bespoke-after' class to all slides after active slide", function() {
-				presentation.activate(5);
-				
-				var afterSlides = slides.slice(6);
+			describe("bespoke-before", function() {
 
-				afterSlides.forEach(function(slide) {
-					expect(slide.className).toMatch(/bespoke-after(\s|$)/);
+				it("should add a 'bespoke-after' class to all slides after active slide", function() {
+					presentation.activate(5);
+					
+					var afterSlides = slides.slice(6);
+
+					afterSlides.forEach(function(slide) {
+						expect(slide.className).toMatch(/bespoke-after(\s|$)/);
+					});
 				});
-			});
 
-			it("should not add a 'bespoke-after' class to all slides before active slide", function() {
-				presentation.activate(5);
-				
-				var notAfterSlides = slides.slice(0, 5);
+				it("should not add a 'bespoke-after' class to all slides before active slide", function() {
+					presentation.activate(5);
+					
+					var notAfterSlides = slides.slice(0, 5);
 
-				notAfterSlides.forEach(function(slide) {
-					expect(slide.className).not.toMatch(/bespoke-after(\s|$)/);
+					notAfterSlides.forEach(function(slide) {
+						expect(slide.className).not.toMatch(/bespoke-after(\s|$)/);
+					});
 				});
+
 			});
 
 		});
