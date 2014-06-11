0c96837e9481ccaa48d6921cba60c7daaee50a6f
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2014-06-12 07:58:46 +1000

    Clean up Karma config and dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -1,12 +1,7 @@
-// Karma configuration
-// http://karma-runner.github.io/0.10/config/configuration-file.html
-
 module.exports = function(config) {
   config.set({
-    // base path, that will be used to resolve files and exclude
     basePath: '',
 
-    // testing framework to use (jasmine/mocha/qunit/...)
     frameworks: ['jasmine', 'browserify'],
 
     files: [
@@ -28,25 +23,12 @@ module.exports = function(config) {
 
     port: 8080,
 
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: false,
 
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
     browsers: ['PhantomJS'],
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
     singleRun: true
   });
 };
