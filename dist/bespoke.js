641308a02c3bd6b6dd77cc23ef3432447fa9753d
Mark Dalgleish <mark.john.dalgleish@gmail.com>
2014-06-23 08:14:04 +1000

    Remove global deck interaction methods

--- dist/bespoke.js
+++ dist/bespoke.js
@@ -7,97 +7,81 @@
  */
 
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.bespoke=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
-var decks = [],
+var from = function(selectorOrElement, plugins) {
+  var parent = selectorOrElement.nodeType === 1 ? selectorOrElement : document.querySelector(selectorOrElement),
+    slides = [].filter.call(parent.children, function(el) { return el.nodeName !== 'SCRIPT'; }),
+    activeSlide = slides[0],
+    listeners = {},
 
-  from = function(selectorOrElement, plugins) {
-    var parent = selectorOrElement.nodeType === 1 ? selectorOrElement : document.querySelector(selectorOrElement),
-      slides = [].filter.call(parent.children, function(el) { return el.nodeName !== 'SCRIPT'; }),
-      activeSlide = slides[0],
-      listeners = {},
+    activate = function(index, customData) {
+      if (!slides[index]) {
+        return;
+      }
 
-      activate = function(index, customData) {
-        if (!slides[index]) {
-          return;
-        }
+      fire('deactivate', createEventData(activeSlide, customData));
+      activeSlide = slides[index];
+      fire('activate', createEventData(activeSlide, customData));
+    },
 
-        fire('deactivate', createEventData(activeSlide, customData));
-        activeSlide = slides[index];
-        fire('activate', createEventData(activeSlide, customData));
-      },
+    slide = function(index, customData) {
+      if (arguments.length) {
+        fire('slide', createEventData(slides[index], customData)) && activate(index, customData);
+      } else {
+        return slides.indexOf(activeSlide);
+      }
+    },
 
-      slide = function(index, customData) {
-        if (arguments.length) {
-          fire('slide', createEventData(slides[index], customData)) && activate(index, customData);
-        } else {
-          return slides.indexOf(activeSlide);
-        }
-      },
+    step = function(offset, customData) {
+      var slideIndex = slides.indexOf(activeSlide) + offset;
 
-      step = function(offset, customData) {
-        var slideIndex = slides.indexOf(activeSlide) + offset;
+      fire(offset > 0 ? 'next' : 'prev', createEventData(activeSlide, customData)) && activate(slideIndex, customData);
+    },
 
-        fire(offset > 0 ? 'next' : 'prev', createEventData(activeSlide, customData)) && activate(slideIndex, customData);
-      },
+    on = function(eventName, callback) {
+      (listeners[eventName] || (listeners[eventName] = [])).push(callback);
 
-      on = function(eventName, callback) {
-        (listeners[eventName] || (listeners[eventName] = [])).push(callback);
-
-        return function() {
-          listeners[eventName] = listeners[eventName].filter(function(listener) {
-            return listener !== callback;
-          });
-        };
-      },
-
-      fire = function(eventName, eventData) {
-        return (listeners[eventName] || [])
-          .reduce(function(notCancelled, callback) {
-            return notCancelled && callback(eventData) !== false;
-          }, true);
-      },
-
-      createEventData = function(el, eventData) {
-        eventData = eventData || {};
-        eventData.index = slides.indexOf(el);
-        eventData.slide = el;
-        return eventData;
-      },
-
-      deck = {
-        on: on,
-        fire: fire,
-        slide: slide,
-        next: step.bind(null, 1),
-        prev: step.bind(null, -1),
-        parent: parent,
-        slides: slides
+      return function() {
+        listeners[eventName] = listeners[eventName].filter(function(listener) {
+          return listener !== callback;
+        });
       };
+    },
 
-    (plugins || []).forEach(function(plugin) {
-      plugin(deck);
-    });
+    fire = function(eventName, eventData) {
+      return (listeners[eventName] || [])
+        .reduce(function(notCancelled, callback) {
+          return notCancelled && callback(eventData) !== false;
+        }, true);
+    },
 
-    activate(0);
+    createEventData = function(el, eventData) {
+      eventData = eventData || {};
+      eventData.index = slides.indexOf(el);
+      eventData.slide = el;
+      return eventData;
+    },
 
-    decks.push(deck);
-
-    return deck;
-  },
-
-  callOnAllDecks = function(method) {
-    return function() {
-      var args = arguments;
-      decks.map(function(deck) {
-        deck[method].apply(null, args);
-      });
+    deck = {
+      on: on,
+      fire: fire,
+      slide: slide,
+      next: step.bind(null, 1),
+      prev: step.bind(null, -1),
+      parent: parent,
+      slides: slides
     };
-  };
+
+  (plugins || []).forEach(function(plugin) {
+    plugin(deck);
+  });
+
+  activate(0);
+
+  return deck;
+};
 
 module.exports = {
-  from: from,
-  slide: callOnAllDecks('slide'),
-  next: callOnAllDecks('next'),
-  prev: callOnAllDecks('prev')
+  from: from
 };
 
 },{}]},{},[1])
